3ebd8b87bc9b19d272d944916df886059eefd2ed
fede <federicobustamantevyp@gmail.com>
2024-05-20 13:58:58 -0300

    Inicio del Proyecto

---
+++ Pages/Actividades.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MesasExamenes.Pages
+{
+    public class PrivacyModels : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        
+        public PrivacyModels(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
---
+++ Pages/Aniversario.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MesasdeExamenes.Pages
+{
+    public class _35aniversarioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
